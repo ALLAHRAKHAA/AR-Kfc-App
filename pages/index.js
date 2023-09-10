7f5fd1506bdfd70e2fc0eca8e3da22047761f012
ALLAHRAKHAA <allahr479@gmail.com>
2023-09-10 22:35:48 +0500

    init basic app

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import App1 from './pages/App1';
+import Canvas from './Components/canvas1';
+import Canvas1 from './Components/canvas1';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App1 />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
